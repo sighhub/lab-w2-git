78f4ec241294d2428b60b87a5f77717f470d36a5
Dan Hicks <hicks.daniel.j@gmail.com>
2020-08-28 10:32:15 -0700

    filling in history and checkout

--- lab.R
+++ lab.R
@@ -1,5 +1,5 @@
 #' ---
-#' title: "Data Science Methods, Lab for Week 99"
+#' title: "Data Science Methods, Lab for Week 2: Git"
 #' author: "Dan Hicks"
 #' email: hicks.daniel.j@gmail.com
 #' output:
@@ -7,15 +7,48 @@
 #'     toc: true
 #' ---
 
-
-## Setup
-## **IMPORTANT**: Add all dependencies to `DESCRIPTION`
-
-## To check your answers locally, run the following: 
-## testthat::test_dir('tests')
+#+ setup, echo=FALSE
+## NB You probably don't want to edit anything here
+library(knitr)
+opts_chunk$set(out.width='40%', echo=FALSE, fig.align='center')
 
 
-
+#' This lab introduces you to navigating histories on GitHub, `git checkout`, and `git branch`, as well as the git side of the workflow we'll use for labs.  
+#' 
+#' *Summary*: 
+#' 1. Use `git checkout` to go back to a previous commit. 
+#' 2. 
+#' 
 #' # Problem 1 #
-#' Assign the value `2L` to `foo`. 
-foo = 2L
+#' Navigate to the GitHub repo for this lab assignment, <https://github.com/data-science-methods/lab-w2-git>.  Fork the lab, then clone it to your working machine.  Open the file `lab.R` in a *new* R session.  
+#' 
+#' # Problem 2 #
+#' Edit `lab.R`.  You don't need to do anything in particular, but they should be trackable changes by it.  Add and commit the changes to your working machine.  
+#' 
+#' # Problem 3 #
+#' Edit `lab.R` again.  Add and commit the changes to your working machine, *and then* push them to your GitHub fork.  
+#' 
+#' # Problem 4 #
+#' 1. Open your fork of the lab repo on GitHub.  The URL will be something like <https://github.com/username/lab-w2-git>.  
+#' 2. Find and click on the history button, above the file list and to the right. (I wasn't able to find a keyboard shortcut for this. :-( )
+
+#+ fig.cap="The history button in GitHub is immediately above the file list, on the right"
+include_graphics(file.path('img', 'history.png'))
+
+#' 3. Look through the history for your commit from problem 2.  Click on the *comment* to view the *diff*, or the changes made by that commit.  
+#' 4. Back on the history page, find the *hash* on the right side.  It should be a series of 7 letters and numbers.  This is an ID code that git uses to track each individual commit.  Click on the clipboard icon to copy the hash. 
+
+#+ fig.cap="The hash for a commit is the series of 7 letters on the right side of the entry in the history view."
+include_graphics(file.path('img', 'hash.png'))
+
+#' # Problem 5 #
+#' 1. Back on your working machine, enter the following into the command line, pasting your hash in place of the example. 
+#' 
+#' ```
+#' $ git checkout 5fc4a1cbe6331e1a38a70ed49d7bfc8182461bc2
+#' ```
+#' 
+#' 2. Look at `lab.R`. Your edits from Problem 3 are gone! 
+#' 3. To reverse this, either find the hash from 
+
+
